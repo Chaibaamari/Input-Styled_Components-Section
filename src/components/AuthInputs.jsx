0f071635b0d7cf8d4419167bd6c784cb8af31c63
Chaibaamari <chaibaamari2@gmail.com>
2024-06-29 12:42:41 +0100

    Section Styled Components using inlineStyle and styledComonents and Dinamique Conditionelle Styling and TelwindCss

---
+++ src/components/AuthInputs.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import { styled } from 'styled-components'
+import Input from './Input';
+
+const Controle = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+export default function AuthInputs() {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleInputChange(identifier, value){
+    if (identifier === 'email') {
+      setEnteredEmail(value);
+    } else {
+      setEnteredPassword(value);
+    }
+  }
+
+  function handleLogin() {
+    setSubmitted(true);
+  }
+
+  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const passwordNotValid = submitted && enteredPassword.trim().length < 6;
+
+  return (
+    <div id="auth-inputs">
+      <Controle>
+        <Input
+          invalid={emailNotValid}
+          label="Email"
+          type="email"
+          onChange={(event) =>
+            handleInputChange("password", event.target.value)
+          }
+        />
+        <Input
+            label="password"
+            type="password"
+            invalid={passwordNotValid}
+            onChange={(event) =>
+              handleInputChange("password", event.target.value)
+            }
+          />
+      </Controle>
+      <div className="actions">
+        <button type="button" className="text-button">
+          Create a new account
+        </button>
+        <button className="button" onClick={handleLogin}>
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+}
